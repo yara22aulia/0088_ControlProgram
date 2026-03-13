2ec28657af683895de7d5eb2f32ef092c8e2f8d5
yara22aulia <yara22aulia@gmail.com>
2026-03-14 05:33:59 +0700

    simplelf.cpp

---
+++ simplelf.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x;
+
+    srand(time(0));
+    x = rand() % 10;
+
+    if (x==5){
+        x = x+1;
+        cout<<"nilai x sudah dikoreksi menjadi : "<<x<<endl;
+    }
+    cout << "Bilangan yang diberikan = "<<x;
+}
