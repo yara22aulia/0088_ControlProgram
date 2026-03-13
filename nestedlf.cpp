47ee8760ddb072ebdd2c7df5c8da5bd3a7e53ef3
yara22aulia <yara22aulia@gmail.com>
2026-03-14 05:27:20 +0700

    nestedlf.cpp

---
+++ nestedlf.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+float Rata_rata(float a, float b){
+    return (a+b)/2;
+}
+
+string Status_lulus(float r){
+    if (r >= 60)
+        if(r > 80){
+            return "Lulus Cumlade";
+        }
+        else{
+            return "Lulus Baik";
+        }
+        else
+        return "Gagal";
+}
+
+int main(){
+    float nilBI,nilMT,rerata;
+    string status;
+
+    cout <<"MAsukan nilai Bahasa Inggris = ";
+    cin >> nilBI;
+    cout <<"Masukan nilai Matematika";
+    cin >> nilMT;
+
+    //rerata = (nilBI+nilMT)/2;
+    //if (rerata >= 60)
+    //    status = "Lolos";
+    //else
+    // status = "Gagal";
+    rerata = Rata_rata(nilBI,nilMT);
+    // status = Status_lulus(rerata);
+
+    cout << "Status kelulusan = "<<Status_lulus(rerata)<<" ,dengan nilai rerata = "<<rerata<<endl;
+}
