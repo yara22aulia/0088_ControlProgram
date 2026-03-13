86a11bd3a2a9239fc703ea1c693425eea2982fef
yara22aulia <yara22aulia@gmail.com>
2026-03-14 04:46:42 +0700

    elself

---
+++ elself.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+float Rata_rata(float a, float b){
+    return (a+b)/2;
+}
+
+string Status_lulus(float r){
+    if (r >= 60)
+    return "Lolos";
+    else 
+    return "Gagal";
+}
+
+int main(){
+    float nilBI, nilMT,rerata;
+    string status;
+
+    cout <<"Masukan nilai Bahasa Inggris = ";
+    cin >> nilBI;
+    cout <<"Masukan nilai Matematika = ";
+    cin >> nilMT;
+
+    //rerata = (nilBI+nilMT)/2;
+    //if (rerata >= 60)
+    //    status = "Lolos"
+    //else
+      //status = "Gagal"
+    rerata = Rata_rata(nilBI,nilMT);
+    //status = Status_lulus(rerata);
+
+    cout << "Status kelulusan = "<<Status_lulus(rerata)<<" ,dengan nilai rerata = "<<rerata<<endl;
+
+}
